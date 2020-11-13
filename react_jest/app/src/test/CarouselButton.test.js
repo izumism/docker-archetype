f7272d658c421f2403e84bbbf0698b7ab1fbe7d4
Shioh Izumi <shioh0716iz@gmail.com>
2020-11-13 17:45:28 +0900

    add CarouselButton functionalities and its test

--- react_jest/app/src/test/CarouselButton.test.js
+++ react_jest/app/src/test/CarouselButton.test.js
@@ -6,9 +6,28 @@ import CarouselButton from '../CarouselButton';
 configure({ adapter: new Adapter() });
 
 describe('CarouselButton', () => {
+  const text = 'Button text';
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<CarouselButton>{text}</CarouselButton>);
+  });
+  
   it('renders a <button>', () => {
-    const text = 'Button text';
-    const wrapper = shallow(<CarouselButton>{text}</CarouselButton>);
     expect(wrapper.type()).toBe('button');
+  });
+
+  it('passes `children` through to the <button>', () => {
+    expect(wrapper.prop('children')).toBe(text);
+  });
+
+  it('passes other props through to the <button>', () => {
+    const onClick = () => {};
+    const className = 'my-carousel-button';
+    const dataAction = 'prep';
+    wrapper.setProps({ onClick, className, 'data-action': dataAction });
+    expect(wrapper.prop('onClick')).toBe(onClick);
+    expect(wrapper.prop('className')).toBe(className);
+    expect(wrapper.prop('data-action')).toBe(dataAction);
   });
 });
